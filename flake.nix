58cd6cfedcf3770587f49ed03426a3212044cbaf
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-03-17 19:58:44 +0800

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, nixos-generators, ... }: { let
+  architectures = builtins.fromJSON (builtins.readFile ./architectures.json);
+  configurations = builtins.map (filename: builtins.replaceStrings [".nix"] [""] filename) (builtins.attrNames (builtins.readDir ./configurations));
+  in
+    nixosModules.customFormats = {config, lib, ...}: {
+      formatConfigs.amazon = {config, lib, ...}: {
+        amazonImage.sizeMB = "auto";
+      };
+
+      formatConfigs.docker = {config, lib, ...}: {
+        services.resolved.enable = false;
+      };
+
+      formatConfigs.oracle = {config, modulesPath, ...}: {
+        imports = [
+          "${toString modulesPath}/virtualisation/oci-image.nix"
+        ];
+
+        formatAttr = "OCIImage";
+        fileExtension = ".qcow2";
+      };
+	  
+      formatConfigs.qcow = {config, lib, ...}: {
+        services.qemuGuest.enable = true;
+      };
+    }; 
+    nixosConfigurations = builtins.listToAttrs (nixpkgs.lib.lists.forEach (nixpkgs.lib.attrsets.cartesianProductOfSets { architecture = architectures; configuration = configurations; }) (systemInfo: nixpkgs.lib.attrsets.nameValuePair "nixos-${systemInfo.configuration}-${systemInfo.architecture}" (nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-generators.nixosModules.all-formats
+        self.nixosModules.customFormats
+        nixpkgs.lib.path.append ./configurations "${systemInfo.configuration}.nix"
+        { nixpkgs.crossSystem = "${systemInfo.architecture}-linux"; }
+      ];
+    })));
+  };
+}
