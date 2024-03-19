69230d4004a9d2590f932d873fab267266329b23
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-03-19 21:28:40 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
