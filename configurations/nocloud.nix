c056c85feffc4a28dc276bfef6ed9003d7b32c63
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-03-20 08:06:15 +0800

    Create nocloud.nix

---
+++ configurations/nocloud.nix
@@ -0,0 +1,23 @@
+{ lib, config, pkgs, modulesPath, ... }: {
+      #https://bugs.launchpad.net/cirros/+bug/1312199
+      boot.kernelParams = [ "no_timer_check" ];
+
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+      networking = {
+        hostName = "nixos";
+      };
+
+      services.openssh.enable = true;
+
+      security.sudo.wheelNeedsPassword = false;
+
+      users.users.nixos = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+      };
+
+      networking.useNetworkd= true;
+
+      systemd.network.enable = true;
+}  
