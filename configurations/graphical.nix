613f3732a81487ff604a1413dbf17872e5e22f6e
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-23 20:36:42 +0800

    Update graphical.nix

--- configurations/graphical.nix
+++ configurations/graphical.nix
@@ -3,6 +3,5 @@
         ./nocloud.nix
       ];
       services.xserver.enable = true;
-      services.xserver.displayManager.sddm.enable = true;
-      services.xserver.desktopManager.plasma5.enable = true;
+      services.xserver.desktopManager.lxqt.enable = true;
 }
