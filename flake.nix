de50815a5a8ac006232ba96b86f4fb2ed4d46105
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-12-03 14:01:39 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
       };
 
       formatConfigs.vagrant = {config, lib, ...}: {
-        image.fileName = "nixos.box";
+        image.extension = lib.mkOverride 99 ".box";
       };
     };
     nixosConfigurations = builtins.listToAttrs (nixpkgs.lib.lists.forEach (nixpkgs.lib.attrsets.cartesianProductOfSets { architecture = architectures; configuration = configurations; }) (systemInfo: nixpkgs.lib.attrsets.nameValuePair "nixos-${systemInfo.configuration}-${systemInfo.architecture}" (nixpkgs.lib.nixosSystem {
