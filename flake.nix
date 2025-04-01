7fe3331ffa89e9730a1827ed3b1f7b13088b4e32
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-04-01 10:57:30 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -51,6 +51,7 @@
     nixosConfigurations = builtins.listToAttrs (nixpkgs.lib.lists.forEach (nixpkgs.lib.attrsets.cartesianProductOfSets { architecture = architectures; configuration = configurations; }) (systemInfo: nixpkgs.lib.attrsets.nameValuePair "nixos-${systemInfo.configuration}-${systemInfo.architecture}" (nixpkgs.lib.nixosSystem {
       system = "${systemInfo.architecture}-linux";
       modules = [
+        (nixpkgs.outPath + "/nixos/modules/profiles/minimal.nix")
         nixos-generators.nixosModules.all-formats
         self.nixosModules.customFormats
         (nixpkgs.lib.path.append ./configurations "${systemInfo.configuration}.nix")
