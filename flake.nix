0682b47e42d97ad049b1fd0da49e43a044df3bb4
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2026-05-22 23:29:54 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { self, nixpkgs, nixos-generators, ... }: let
+  outputs = { self, nixpkgs, ... }: let
   architectures = builtins.fromJSON (builtins.readFile ./architectures.json);
   configurations = builtins.map (filename: builtins.replaceStrings [".nix"] [""] filename) (builtins.attrNames (builtins.readDir ./configurations));
   in
   {
-    nixosModules.customFormats = {config, lib, ...}: {
-      formatConfigs.amazon = {config, lib, ...}: {
-        amazonImage.sizeMB = "auto";
-      };
+    nixosModules.formatFixes = {config, lib, modulesPath, ...}: {
+      image.modules =
+        docker = {
+          services.resolved.enable = false;
+        };
 
-      formatConfigs.do = {config, lib, ...}: {
-        # https://github.com/NixOS/nixpkgs/issues/308404
-        # set both, else module merge rules lead to duplicate entries in boot.loader.grub.devices
-        boot.loader.grub.devices = lib.mkForce [ "/dev/vda" ];
-        boot.loader.grub.device = "/dev/vda";
-      };
-
-      formatConfigs.docker = {config, lib, ...}: {
-        services.resolved.enable = false;
-      };
-
-      formatConfigs.oracle = {config, modulesPath, ...}: {
-        imports = [
-          "${toString modulesPath}/virtualisation/oci-image.nix"
-        ];
+        oracle = {
+          imports = [
+            "${toString modulesPath}/virtualisation/oci-image.nix"
+          ];
 
         formatAttr = "OCIImage";
         fileExtension = ".qcow2";
@@ -40,13 +26,13 @@
         services.qemuGuest.enable = true;
       };
     }; 
-    nixosConfigurations = builtins.listToAttrs (nixpkgs.lib.lists.forEach (nixpkgs.lib.attrsets.cartesianProductOfSets { architecture = architectures; configuration = configurations; }) (systemInfo: nixpkgs.lib.attrsets.nameValuePair "nixos-${systemInfo.configuration}-${systemInfo.architecture}" (nixpkgs.lib.nixosSystem {
+    nixosConfigurations = builtins.listToAttrs (nixpkgs.lib.lists.forEach (nixpkgs.lib.attrsets.cartesianProduct { architecture = architectures; configuration = configurations; }) (systemInfo: nixpkgs.lib.attrsets.nameValuePair "nixos-${systemInfo.configuration}-${systemInfo.architecture}" (nixpkgs.lib.nixosSystem {
       system = "${systemInfo.architecture}-linux";
       modules = [
-        nixos-generators.nixosModules.all-formats
-        self.nixosModules.customFormats
+        self.nixosModules.formatFixes
         (nixpkgs.lib.path.append ./configurations "${systemInfo.configuration}.nix")
       ];
     })));
+	
   };
 }
